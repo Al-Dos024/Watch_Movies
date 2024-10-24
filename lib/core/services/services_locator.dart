476be857e5a8201c7d6fedb4b67e8bb26d9b9113
Ali Dosoqi <106039568+Al-Dos024@users.noreply.github.com>
2024-10-24 22:00:49 +0300

    add bloc details to the project

--- lib/core/services/services_locator.dart
+++ lib/core/services/services_locator.dart
@@ -2,9 +2,12 @@ import 'package:get_it/get_it.dart';
 import 'package:watch_movies/movies/data/datasource/movie_remote_data_source.dart';
 import 'package:watch_movies/movies/data/repository/movies_repository.dart';
 import 'package:watch_movies/movies/domain/repository/base_movies_repository.dart';
+import 'package:watch_movies/movies/domain/usecases/get_movie_details_usecase.dart';
 import 'package:watch_movies/movies/domain/usecases/get_now_playing_movies_usecase.dart';
 import 'package:watch_movies/movies/domain/usecases/get_popular_movies_usecase.dart';
+import 'package:watch_movies/movies/domain/usecases/get_recommendation_usecase.dart';
 import 'package:watch_movies/movies/domain/usecases/get_top_rated_movies_usecase.dart';
+import 'package:watch_movies/movies/presentation/controller/movie_details_bloc.dart';
 import 'package:watch_movies/movies/presentation/controller/movies_bloc.dart';
 
 final sl = GetIt.instance;
@@ -13,11 +16,14 @@ class ServicesLocator {
   void init() {
     /// Bloc
     sl.registerFactory(() => MoviesBloc(sl(), sl(), sl()));
+    sl.registerFactory(() => MovieDetailsBloc(sl(), sl()));
 
     /// Use Cases
     sl.registerLazySingleton(() => GetNowPlayingMoviesUseCase(sl()));
     sl.registerLazySingleton(() => GetPopularMoviesUseCase(sl()));
     sl.registerLazySingleton(() => GetTopRatedMoviesUseCase(sl()));
+    sl.registerLazySingleton(() => GetMovieDetailsUseCase(sl()));
+    sl.registerLazySingleton(() => GetRecommendationUseCase(sl()));
 
     /// Repository
     sl.registerLazySingleton<BaseMoviesRepository>(
